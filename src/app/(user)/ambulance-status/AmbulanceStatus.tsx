7574c4d3d5cb631e682dde6ff3634b72aa14e459
Yushan Bhattarai <np03cs4m210039@heraldcollege.edu.np>
2023-09-13 12:02:00 +0545

    Linked ambulance called by me status

--- src/app/(user)/ambulance-status/AmbulanceStatus.tsx
+++ src/app/(user)/ambulance-status/AmbulanceStatus.tsx
@@ -1,11 +1,16 @@
+import Link from "next/link";
+
 export default function AmbulanceStatus() {
   return (
     <div className="m-auto flex max-w-[600px] flex-col items-center justify-center gap-4 rounded-3xl bg-[#E2DEDE] p-12">
       <p className="text-3xl font-extrabold">Ambulance Status</p>
       <div className="flex flex-col gap-2">
-        <button className="rounded-2xl bg-white px-4 py-2 shadow-lg transition-colors hover:bg-gray-100">
+        <Link
+          href="/ambulance-status/1"
+          className="w-full rounded-2xl bg-white px-4 py-2 text-center shadow-lg transition-colors hover:bg-gray-100"
+        >
           An Ambulance was called by me
-        </button>
+        </Link>
         <button className="rounded-2xl bg-white px-4 py-2 shadow-lg transition-colors hover:bg-gray-100">
           An Ambulance was called by others
         </button>
