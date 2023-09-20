0cce5a319a510bc3249e5db176a5871480bed5e5
Yushan Bhattarai <np03cs4m210039@heraldcollege.edu.np>
2023-09-20 16:12:00 +0545

    Added dashboard request details page

--- src/app/(admin)/dashboard/AdminInfo.tsx
+++ src/app/(admin)/dashboard/AdminInfo.tsx
@@ -3,8 +3,13 @@ import PublicAvatar from "@/../public/images/public_avatar.png";
 
 export default function AdminInfo() {
   return (
-    <div className="flex gap-3 items-center">
-      <Image src={PublicAvatar} width={80} className="rounded-full" alt="Avatar" />
+    <div className="flex items-center gap-3">
+      <Image
+        src={PublicAvatar}
+        width={80}
+        className="rounded-full"
+        alt="Avatar"
+      />
       <div className="flex flex-col gap-1">
         <p className="text-2xl text-[#54657E]">Anna Parker</p>
         <p>Position: Admin</p>
