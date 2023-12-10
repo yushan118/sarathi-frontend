e47e5121e05828b0f466d4af0c35526b0543493f
Yushan Bhattarai <np03cs4m210039@heraldcollege.edu.np>
2023-12-11 03:04:00 +0545

    Fixed build error

--- src/helper/auth.ts
+++ src/helper/auth.ts
@@ -4,4 +4,6 @@ import { redirect } from "next/navigation";
 export function redirectWithCurrentCallbackUrl(url: string) {
   const headersList = headers();
   redirect(`${url}?redirect=${headersList.get("x-pathname")}`);
+
+  return null;
 }
