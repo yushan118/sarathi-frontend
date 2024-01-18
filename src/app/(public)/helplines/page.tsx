697026c345d6733c014b72715b5d16aa33a4582e
Yushan Bhattarai <np03cs4m210039@heraldcollege.edu.np>
2024-01-18 18:23:00 +0545

    Helplines, Our Services: Added content

--- src/app/(public)/helplines/page.tsx
+++ src/app/(public)/helplines/page.tsx
@@ -7,15 +7,60 @@ export default function NewsPage() {
       <h1 className="mb-4 text-4xl font-semibold">Helplines</h1>
       <div className="flex flex-col items-center gap-8">
         <Image src={Ambulance1} alt="Ambulance 1" width={640} height={360} />
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi maximus
-          malesuada sagittis. Mauris ut pellentesque orci, vel pretium elit.
-          Donec mattis felis ut dui aliquam, eget molestie quam euismod.
-          Vestibulum nisi neque, porttitor at dignissim sed, consequat imperdiet
-          dui. Vestibulum tincidunt sem lobortis felis pulvinar porta. Morbi et
-          metus maximus, ultrices nisi eu, maximus sapien. Integer cursus libero
-          quam, eu dictum ex tincidunt et.
-        </p>
+        <table>
+          <thead className="bg-[#DDDDDD] font-semibold">
+            <tr>
+              <td>Name</td>
+              <td>Telephone Numbers</td>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Police Control</td>
+              <td>100</td>
+            </tr>
+            <tr>
+              <td>Police Emergency Number</td>
+              <td>4228435/4226853</td>
+            </tr>
+            <tr>
+              <td>Metropolitan Police Range (Kathmandu)</td>
+              <td>4261945/4261790</td>
+            </tr>
+            <tr>
+              <td>Metropolitan Police Range (Lalitpur)</td>
+              <td>5521207</td>
+            </tr>
+            <tr>
+              <td>Metropolitan Police Range (Bhaktapur)</td>
+              <td>6614821</td>
+            </tr>
+            <tr>
+              <td>Paropakar Ambulance Service</td>
+              <td>4260859</td>
+            </tr>
+            <tr>
+              <td>Lalitpur Redcross Ambulance Service</td>
+              <td>5545666</td>
+            </tr>
+            <tr>
+              <td>Bishal Bazar Ambulance Service</td>
+              <td>4244121</td>
+            </tr>
+            <tr>
+              <td>Redcross Ambulance Service</td>
+              <td>4228094</td>
+            </tr>
+            <tr>
+              <td>Agrawal Sewa Centre</td>
+              <td>4424875</td>
+            </tr>
+            <tr>
+              <td>Aasara Drug Rehabilitation Center</td>
+              <td>01-4384881</td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     </main>
   );
--- src/app/(public)/services/page.tsx
+++ src/app/(public)/services/page.tsx
@@ -10,39 +10,34 @@ export default function NewsPage() {
       <div className="flex flex-col items-start gap-8">
         <div className="flex flex-col items-center gap-12 md:flex-row">
           <Image src={Ambulance1} alt="Ambulance 1" width={640} height={360} />
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi
-            maximus malesuada sagittis. Mauris ut pellentesque orci, vel pretium
-            elit. Donec mattis felis ut dui aliquam, eget molestie quam euismod.
-            Vestibulum nisi neque, porttitor at dignissim sed, consequat
-            imperdiet dui. Vestibulum tincidunt sem lobortis felis pulvinar
-            porta. Morbi et metus maximus, ultrices nisi eu, maximus sapien.
-            Integer cursus libero quam, eu dictum ex tincidunt et.
-          </p>
+          <ul className="list-disc list-inside">
+            <li>Boat ambulance</li>
+            <li>Intensive care ambulance</li>
+            <li>Air ambulance</li>
+            <li>Box ambulance</li>
+            <li>Nontransporting ems vehicle</li>
+            <li>Helicopter</li>
+          </ul>
         </div>
         <div className="flex flex-col items-center gap-12 md:flex-row-reverse">
           <Image src={Ambulance2} alt="Ambulance 2" width={640} height={360} />
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi
-            maximus malesuada sagittis. Mauris ut pellentesque orci, vel pretium
-            elit. Donec mattis felis ut dui aliquam, eget molestie quam euismod.
-            Vestibulum nisi neque, porttitor at dignissim sed, consequat
-            imperdiet dui. Vestibulum tincidunt sem lobortis felis pulvinar
-            porta. Morbi et metus maximus, ultrices nisi eu, maximus sapien.
-            Integer cursus libero quam, eu dictum ex tincidunt et.
-          </p>
+          <ul className="list-disc list-inside">
+            <li>Basic Life Support ambulance</li>
+            <li>Patient transport</li>
+            <li>Mortuary ambulances</li>
+            <li>Life support</li>
+            <li>Bus</li>
+            <li>MVA Ambulance</li>
+          </ul>
         </div>
         <div className="flex flex-col items-center gap-12 md:flex-row">
           <Image src={Ambulance3} alt="Ambulance 3" width={640} height={360} />
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi
-            maximus malesuada sagittis. Mauris ut pellentesque orci, vel pretium
-            elit. Donec mattis felis ut dui aliquam, eget molestie quam euismod.
-            Vestibulum nisi neque, porttitor at dignissim sed, consequat
-            imperdiet dui. Vestibulum tincidunt sem lobortis felis pulvinar
-            porta. Morbi et metus maximus, ultrices nisi eu, maximus sapien.
-            Integer cursus libero quam, eu dictum ex tincidunt et.
-          </p>
+          <ul className="list-disc list-inside">
+            <li>Bariatric ambulance</li>
+            <li>Neonatal ambulance</li>
+            <li>Neonatology</li>
+            <li>Emergency care practitioner vehicle</li>
+          </ul>
         </div>
       </div>
     </main>
