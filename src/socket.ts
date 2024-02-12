68bdb9ce95055ecdefb5e577228b196a97b8a375
Yushan Bhattarai <np03cs4m210039@heraldcollege.edu.np>
2024-02-13 02:54:01 +0545

    Updated socket url

--- src/socket.ts
+++ src/socket.ts
@@ -1,5 +1,6 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:8080", {
+export const socket = io("http://sarathiii.com:8080", {
   autoConnect: false,
+  secure: false,
 });
