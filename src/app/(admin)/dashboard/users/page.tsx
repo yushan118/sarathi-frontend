80e4e215ee2c2ebe06c9fce47fc9ffb6e4d49378
Yushan Bhattarai <bhattarai94yushan@gmail.com>
2024-02-20 23:21:10 +0545

    Comments Added-1

--- src/app/(admin)/dashboard/users/page.tsx
+++ src/app/(admin)/dashboard/users/page.tsx
@@ -1,10 +1,19 @@
+// Importing the fetchWithAuth function for authenticated API calls
 import fetchWithAuth from "@/helper/fetch";
+
+// Importing the UserEdit component for editing user details
 import UserEdit from "./Edit";
 
+// Async function for rendering the UsersPage component
 export default async function UsersPage() {
+
+  // Fetching the list of users with authentication
   const usersListRes = await fetchWithAuth("/user/list", "ADMIN_", {
+    
     next: { tags: ["users-list"] },
   });
+
+  // Parsing the JSON response containing the list of users
   const usersList: {
     _id: string;
     name: string;
@@ -13,14 +22,22 @@ export default async function UsersPage() {
   }[] = await usersListRes.json();
 
   return (
+
+    // Rendering the main content of the UsersPage component
     <div>
+
+      {/* Table displaying user details */}
       <table className="text-left">
+
+         {/* Table header */}
         <thead className="border-b font-medium dark:border-neutral-500">
           <tr>
             <th>Name</th>
             <th>Mobile Number</th>
           </tr>
         </thead>
+
+        {/* Table body with UserEdit components for each user */}
         <tbody>
           {usersList.map((d) => (
             <UserEdit
