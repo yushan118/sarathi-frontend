a2c4c0f470ec97f2d637992ca90bc9282eaa9dee
Yushan Bhattarai <bhattarai94yushan@gmail.com>
2024-02-20 23:25:20 +0545

    Frontend-Admin-Users Comment

--- src/app/(admin)/dashboard/users/server-actions.ts
+++ src/app/(admin)/dashboard/users/server-actions.ts
@@ -1,17 +1,29 @@
 "use server";
 
+// Importing the fetchWithAuth function for authenticated API calls
 import fetchWithAuth from "@/helper/fetch";
+
+// Importing the action and revalidateTag functions for server-side actions
 import { action } from "@/helper/safe-action";
 import { revalidateTag } from "next/cache";
+
+// Importing the zod library for schema validation
 import { z } from "zod";
 
+// Server-side action for editing user details
 export const editUser = action(
+
+  // Input validation using zod schema
   z.object({
     id: z.string(),
     name: z.string(),
     phone_number: z.string().length(10),
   }),
+
+  // Async function to handle the editUser action
   async (input) => {
+
+    // Making a PATCH request to update user details
     await fetchWithAuth("/user/list", "ADMIN_", {
       method: "PATCH",
       headers: {
@@ -23,16 +35,25 @@ export const editUser = action(
         mobile_number: input.phone_number,
       }),
     });
+
+    // Revalidating the "users-list" cache tag to trigger a re-fetch
     revalidateTag("users-list");
   },
 );
 
+// Server-side action for suspending/unsuspending a user
 export const suspendUser = action(
+
+  // Input validation using zod schema
   z.object({
     id: z.string(),
     action: z.enum(["suspend", "unsuspend"]),
   }),
+
+  // Async function to handle the suspendUser action
   async (input) => {
+
+    // Making a POST request to suspend/unsuspend a user
     await fetchWithAuth(`/user/${input.id}`, "ADMIN_", {
       method: "POST",
       headers: {
@@ -42,6 +63,8 @@ export const suspendUser = action(
         action: input.action,
       }),
     });
+
+    // Revalidating the "users-list" cache tag to trigger a re-fetch
     revalidateTag("users-list");
   },
 );
